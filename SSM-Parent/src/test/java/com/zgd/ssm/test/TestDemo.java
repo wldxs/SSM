e0782d2b8287d4752b0944ba4f88f834b90d6e59
wldxs <wldxs01@163.com>
2018-08-20 22:35:49 +0800

    SSM初步测试1

---
+++ SSM-Parent/src/test/java/com/zgd/ssm/test/TestDemo.java
@@ -0,0 +1,11 @@
+package com.zgd.ssm.test;
+
+/**
+ * @author zgd
+ * @date 2018/8/20 22:34
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
